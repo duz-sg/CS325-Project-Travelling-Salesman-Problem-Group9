00320fe953f2f4643c77a3ce6b7ac8845342ec99
duz-sg <deandz@gmail.com>
2017-11-26 16:51:22 +0800

    Use the same distance calculation method as in tsp-verifier.py

--- nearestNeighbor.py
+++ nearestNeighbor.py
@@ -1,20 +1,20 @@
 import math
 
 def findDistance(city1, city2):
-    return int(math.sqrt( ((city1.x - city2.x)**2) + ((city1.y - city2.y)**2) ))
+    return int(round(math.sqrt( ((city1.x - city2.x)**2) + ((city1.y - city2.y)**2) )))
 
 
 def findIndexOfClosestUnvisitedCity(currentCity, listOfCities):
     shortestDistance = float("inf")
     indexOfShortest = None
-    
+
     # Loop through all the cities
     for i in range(0, len(listOfCities)):
-        
+
         # Check if we have a new shortest distance to an unvisited city
         if (listOfCities[i].visited == False and
             findDistance(currentCity, listOfCities[i]) < shortestDistance):
-            
+
             shortestDistance = findDistance(currentCity, listOfCities[i])
             indexOfShortest = i
 
@@ -42,12 +42,12 @@ def nearestNeighborAlgorithm(V, startingIndex):
 
         # keep track of order
         O.append(C.i)
-        
+
     # Add the distance to get back to the starting city
     distanceHome = findDistance(C, V[startingIndex])
     D = D + distanceHome
     return D, O
-    
+
 def repetitiveNearestNeighbor(V):
     # Initialize variables to held the best solutions that we can find
     D = float("inf")
@@ -55,7 +55,7 @@ def repetitiveNearestNeighbor(V):
 
     # Try computing the nearest neighbor algorithm for each possible starting city
     for i in range(0, len(V)):
-        
+
         # reset all cities to unvisited
         # TODO - use a toggle so that this loop is not necessary
         for j in range(0, len(V)):
@@ -63,7 +63,7 @@ def repetitiveNearestNeighbor(V):
 
         # determine path length and order
         d, o = nearestNeighborAlgorithm(V, i)
-        
+
         # store result if it the new best
         if (d < D):
             D = d
