4c32d4bf366a73880e11067858ce0ee8ba7b1773
Trevor Worthey <wortheyt@flip1.engr.oregonstate.edu>
2017-12-01 14:14:44 -0800

    Christofides algorithm appears to be working
    TODO:
    	* Import blossom.py in a way that it can be run directly from christofides.py
    	* Change output type to match tour output format
    	* Integrate with tspDriver.py

--- christofides.py
+++ christofides.py
@@ -1,5 +1,6 @@
 import math
 import sys
+from collections import defaultdict
 # import blossom
 
 def create_graph(input_file):
@@ -90,28 +91,43 @@ def find_induced_graph(graph, MST, odd):
 
 	return induced_graph
 
-def create_multigraph(MST, mwpm):
+def create_multigraph(MST, mwpm, vertices):
 	multigraph = MST + mwpm
-	return multigraph
+
+	degrees = dict()
+
+	for v in vertices:
+		degree = 0
+		for e in multigraph:
+			if v in e:
+				degree += 1
+
+		degrees[v] = degree
+
+	return multigraph, degrees
 
 
 #Adapted from https://github.com/feynmanliang/Euler-Tour/blob/master/FindEulerTour.py
-def find_euler_path(multigraph, vertices):
+def find_euler_path(multigraph, vertices, degrees):
+
 	graph = multigraph
+
 	path = []
 
+	start = graph[0][0]
 	cur_vert = graph[0][0]
 
 	# print "In Euler tour. Length of multigraph:"
 	print len(graph)
 
-	for e in graph:
-		print e
-
 	while len(graph) != 0:
 		print "Top of loop. Length of graph: {}".format(len(graph))
+
 		for e in graph:
 			if cur_vert in e:
+
+
+
 				print "Found edge {}".format(e)
 				cur_edge = e
 				path.append(cur_edge)
@@ -121,9 +137,67 @@ def find_euler_path(multigraph, vertices):
 					cur_vert = cur_edge[0]
 				graph.remove(cur_edge)
 				# print "Length of graph: {}".format(len(graph))
-				continue
 
 	return path
+
+# Adapted from https://github.com/feynmanliang/Euler-Tour/blob/master/FindEulerTour.py
+def find_euler_tour(graph):
+	tour = []
+	E = graph
+	numEdges = defaultdict(int)
+
+	def find_tour(u):
+		for e in range(len(E)):
+			if E[e] == 0:
+				continue
+			if u == E[e][0]:
+				u, v = E[e]
+				E[e] = 0
+				find_tour(v)
+			elif u == E[e][1]:
+				v, u = E[e]
+				E[e] = 0
+				find_tour(v)
+		tour.insert(0, u)
+
+	for e in graph:
+		i, j = e
+		numEdges[i] += 1
+		numEdges[j] += 1
+
+	start = graph[0][0]
+	current = start
+
+	find_tour(current)
+
+	return tour
+
+
+def find_hamil_circuit(path):
+
+	circuit = []
+
+	for v in path:
+		if v not in circuit:
+			circuit.append(v)
+
+	return circuit
+
+
+def calculate_tour(graph, circuit):
+
+	tour_weight = 0
+
+	for i in range(len(circuit)):
+		current = circuit[i]
+		if i == len(circuit) - 1:
+			i = -1
+		else:
+			i += 1
+		next_node = circuit[i]
+		tour_weight += graph[current][next_node]
+
+	return tour_weight
 
 
 f = sys.argv[1]
@@ -131,7 +205,6 @@ graph, vert_dict, edges, vertices = create_graph(f)
 t = create_mst(graph)
 o = find_odd_vertices(t, len(graph))
 i = find_induced_graph(graph, t, o)
-
 
 # for v in i:
 # 	print v
@@ -154,13 +227,21 @@ with open("blossomoutput.txt", "r") as bo:
 		# print "+"
 
 
-mg = create_multigraph(mwpm, t)
+mg, degrees = create_multigraph(mwpm, t, vertices)
 
 # for m in mg:
 # 	print m
 
+ep = find_euler_tour(mg)
 
-ep = find_euler_path(mg, vertices)
+# for e in ep:
+# 	print e
 
-for e in ep:
-	print e
+c = find_hamil_circuit(ep)
+
+# for v in c:
+# 	print v
+
+w = calculate_tour(graph, c)
+
+print w
